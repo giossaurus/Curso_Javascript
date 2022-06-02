6848f5ac61cc63440dc304cc0cbfec029e9c2209
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2022-06-01 22:31:09 -0300

    programa bytebank js

--- modulo 2/index.js
+++ modulo 2/index.js
@@ -1,24 +1,5 @@
-class Cliente{
-    nome;
-    cpf;
-}
-
-class ContaCorrente{
-    agencia;
-    _saldo;
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor >0){
-            this._saldo += valor;
-        }
-    }
-}
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -29,12 +10,14 @@ cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia - 1001;
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.sacar(50);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
+contaCorrenteRicardo.transferir(200, conta2);
+console.log(conta2);
 console.log(contaCorrenteRicardo);
-
-
-
