7bd7d20e8e3c0c643a4c3f1a0df3e0fa74bdac6f
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2022-06-02 19:39:59 -0300

    constructors

--- modulo 2/ContaCorrente.js
+++ modulo 2/ContaCorrente.js
@@ -3,14 +3,30 @@ import { Cliente }  from "./Cliente.js"
 export class ContaCorrente{
     agencia;
     _cliente;
+    _saldo = 0;
+
+
 
     set cliente(novoValor){
-        if(novoValor instanceof Cliente)
-        this._cliente = novoValor;
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
     }
 
-    _saldo = 0;
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
     
+    constructor(cliente, agencia){
+        this.agencia = agencia;
+        this.cliente = cliente;
+    }
+
+
     sacar(valor){
         if(this._saldo >= valor){
             this._saldo -= valor;
@@ -32,5 +48,3 @@ export class ContaCorrente{
 
     }
 }
-
-
