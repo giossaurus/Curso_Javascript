dfac0d2e55e2d56d2250b49456d3a8b875884ae1
giossaurus <91167613+giossaurus@users.noreply.github.com>
2022-06-02 23:34:51 -0300

    suoer

--- modulo 2/ContaCorrente.js
+++ modulo 2/ContaCorrente.js
@@ -3,8 +3,8 @@ import { Conta } from "./Conta.js";
 
 export class ContaCorrente extends Conta{
     static numeroDeContas = 0;
-    
-    constructor(agencia, cliente){
+    constructor(cliente, agencia){
+        super(0, cliente, agencia);
         ContaCorrente.numeroDeContas += 1;
     }
 }
