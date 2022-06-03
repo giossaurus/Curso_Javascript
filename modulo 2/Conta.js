22664b952ca3e9fe6dc95b46c7e04bfe42054684
giossaurus <91167613+giossaurus@users.noreply.github.com>
2022-06-02 23:16:58 -0300

    Update Conta.js

--- modulo 2/Conta.js
+++ modulo 2/Conta.js
@@ -1,15 +1,35 @@
 export class Conta{
-    constructor(saldoInicial, cliente, agencia){
+    constructor(tipo, saldoInicial, cliente, agencia){
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
+        this._this = tipo;
+    }
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
     }
 
     sacar(valor){
-        taxa = 1.1 * valor;
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
+        
+        let = taxa = 1.1 * valor;
+        if(this._tipo == "corrente"){
+            taxa = 1.1;
+        }
+        const valorSacado = taxa * valor;
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado;
+            return valorSacado;
         }
     }
 
