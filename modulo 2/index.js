cf3a291847de05c024341324a1352ba873e139fe
giossaurus <91167613+giossaurus@users.noreply.github.com>
2022-06-06 00:25:36 -0300

    polimorfismo

--- modulo 2/index.js
+++ modulo 2/index.js
@@ -1,16 +1,15 @@
 import {Cliente} from "./Conta/Cliente.js";
-import {ContaCorrente} from "./Conta/ContaCorrente.js";
-import { ContaPoupanca } from "./ContaCorrente/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+import { Gerente } from "./ Funcionarios/Gerente.js";
+import { Diretor } from "./ Funcionarios/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456")
+const gerente = new Gerente ("Ricardo", 5000, 12378945601);
+gerente.cadastrarSenha("123")
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+SistemaAutenticacao.login(gerente, "123456789");
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new contaSalario(cliente1);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
+const estaLogado = SistemaAutenticacao.login(diretor, "123456789");
 
-
-console.log(contaSalario);
+console.log(estaLogado);
