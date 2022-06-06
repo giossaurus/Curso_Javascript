fccfffc50b02756780308800a0fd287537376ba4
giossaurus <91167613+giossaurus@users.noreply.github.com>
2022-06-06 00:11:43 -0300

    funcionarios e diretores

--- modulo 2/index.js
+++ modulo 2/index.js
@@ -1,15 +1,16 @@
-import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
+import {Cliente} from "./Conta/Cliente.js";
+import {ContaCorrente} from "./Conta/ContaCorrente.js";
+import { ContaPoupanca } from "./ContaCorrente/ContaPoupanca.js";
+import { ContaSalario } from "./Conta/ContaSalario.js";
+
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
 const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const contaSalario = new contaSalario(cliente1);
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-contaPoupanca.sacar(10);
 
-console.log(contaPoupanca);
-console.log(contaCorrenteRicardo);
+console.log(contaSalario);
